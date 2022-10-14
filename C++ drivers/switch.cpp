3325beb94aac9d9939122e353d03b8b517c1e165
osamamuhammad3623 <osamamohammed3623@gmail.com>
2022-10-14 18:38:45 +0200

    complete switch class

---
+++ C++ drivers/switch.cpp
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include "switch.h"
+#include "common_macros.h"
+#include "tm4c123gh6pm.h"
+
+BuiltIn_Switch::BuiltIn_Switch(Switch_Pin user_switch) : sw{user_switch}{
+	Configure();
+}
+
+void BuiltIn_Switch::Configure(){
+	SYSCTL_RCGC2_R |= (1 << 5); /* enable clock for port F */
+	CLEAR_BIT(GPIO_PORTF_DIR_R, sw);
+	CLEAR_BIT(GPIO_PORTF_AFSEL_R, sw);
+	GPIO_PORTF_LOCK_R = 0x4C4F434B; /* unlock commit reg. */
+	SET_BIT(GPIO_PORTF_CR_R, sw); /* enable commit reg. to control PUR reg. */
+	SET_BIT(GPIO_PORTF_PUR_R, sw);
+	SET_BIT(GPIO_PORTF_DEN_R, sw);
+}
+
+uint8_t BuiltIn_Switch::isPressed(){
+    if (BIT_IS_CLEAR(GPIO_PORTF_DATA_R,sw))
+        return 1;
+
+    return 0;
+}
---
+++ C++ drivers/switch.h
@@ -0,0 +1,33 @@
+#ifndef BUILTIN_SWITCH_H_
+#define BUILTIN_SWITCH_H_
+
+#include <stdint.h>
+
+typedef enum{
+    SW1=4, SW2=0
+}Switch_Pin;
+
+class BuiltIn_Switch{
+    Switch_Pin sw;
+public:
+
+    BuiltIn_Switch(Switch_Pin user_switch);
+
+    /*
+    * Description: Configure one of the built-in switches
+    *      1.make the pin an input pin
+    *      2.no alternate function
+    *      3.enable pull-up resistor
+    *      3.digital enable
+    * */
+    void Configure();
+
+
+    /*
+    * Description: Check whether a switch is pressed on not.
+    *              return 1 if pressed, 0 otherwise
+    * */
+    uint8_t isPressed(void);
+};
+
+#endif /* BUILTIN_SWITCH_H_ */
